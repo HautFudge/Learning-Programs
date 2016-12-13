df46cf0d957138583ba37e15a2548bb90aa7e86a
Thomp <thomp@protonmail.com>
2016-12-13 05:30:00 -0600

    new study in classes and OOP

---
+++ phone plan cost.py
@@ -0,0 +1,23 @@
+#phone values
+'''
+aspects:
+Phone (name and cost)
+Carrier?
+Plan type (affects rate and phone cost timeframe)
+    Pay as you Go
+    Contract
+
+'''
+
+class Phoneplan:
+    def __init__(self, phone, plan):
+        self.phone = phone
+        self.plan = plan
+
+    def gophone(self):
+        pass
+
+    gophoneplandata = {
+    .5 : 30,
+    3 : 40
+    }
