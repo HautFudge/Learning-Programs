0801833340671f3a6d3c47790be263e3c551276d
Thomp <thomp@protonmail.com>
2017-02-27 15:28:48 -0600

    new java project. on ch. 2 of think java book.

---
+++ javastuff/timey.java
@@ -0,0 +1,23 @@
+public class timey {
+  public static void main(String[] args) {
+
+    int lastHour = 11;
+    int lastMinute = 59;
+    System.out.print("Number of minutes since midnight: ");
+    System.out.println(lastHour * 60 + lastMinute);
+
+    double y = 1 / 3;
+    System.out.println(y);
+
+    double z = 1.0 / 3;
+    System.out.println(z);
+
+    System.out.println("Addition with integers first:");
+    System.out.println(1 + 2 + "Hello");
+
+    System.out.println("Addition with string first:");
+    System.out.println("Hello" + 1 + 2);
+
+
+  }
+}
