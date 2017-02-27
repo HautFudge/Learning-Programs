cd8685858eea342384d01df710364d8ddf81e35a
Thomp <thomp@protonmail.com>
2017-02-27 06:27:41 -0600

    Use this Java folder to build learning java projects.

---
+++ JavaStuff/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    //generate some simple output
+    System.out.print("Hello, World! ");
+    System.out.println("How's it going?");
+    /* Here are some unused variables
+
+    */
+    String message;
+    int hour, minute;
+    message = "Hello!";
+    hour = 11;
+    minute = 59;
+
+    System.out.println("The current time is " + hour + ":" + minute);
+  }
+
+}
